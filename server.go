291e2a06253d77ed3f95f214a7d97cb57ca67f90
wesb <wesb@qualtrics.com>
2015-05-06 22:01:03 -0600

    simple view wiki page

--- server.go
+++ server.go
@@ -9,7 +9,14 @@ func handler(res http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(res, "Hi there, I love %s!", req.URL.Path[1:])
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
+    http.HandleFunc("/view/", viewHandler)
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
